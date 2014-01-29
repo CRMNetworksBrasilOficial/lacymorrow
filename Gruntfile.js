98ce88b28ccfff017e044e6ded62b7844c0627b1
lacymorrow <me@lacymorrow.com>
2014-01-28 23:56:28 -0500

    Complete Boilerplate - 0.1.0 (ng/bootstrap)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,108 @@
 module.exports = function(grunt) {
-
+  require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    banner: '/*!\n' +
+          ' * <%= pkg.name%> v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+          ' * Copyright 2014-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+          ' * Licensed under <%= _.pluck(pkg.licenses, "type") %> (<%= _.pluck(pkg.licenses, "url") %>)\n' +
+          ' */\n',
+    jqueryCheck: 'if (typeof jQuery === \'undefined\') { throw new Error(\'Bootstrap requires jQuery\') }\n\n',
+    /* EXECUTE BUILD ASSET GRUNT - 
+    Remember: to add new assets install to devDependencies (package.json) and copy list below.
+    You are responsible for keeping assets up to date (e.g. git pull) */
+    exec: {
+      ng: {
+        command: '(cd assets/angular.js;npm install;grunt -f)'
+      },
+      bootstrap: {
+        command: '(cd assets/bootstrap;npm install;grunt -f)'
+      }
+    },
+    copy: {
+    /* BUILD ASSETS TO COPY */
+      bootstrap: {
+        expand: true,
+        cwd: 'assets/bootstrap/dist/',
+        src: ['**'],
+        dest: 'dist/assets/bootstrap/'
+      },
+      ng: {
+        expand: true,
+        cwd: 'assets/angular.js/build/',
+        src: ['**'],
+        dest: 'dist/assets/ng/'
+      },
+    /* END OF BUILD ASSETS */
+      app: {
+        expand: true,
+        cwd: 'src/',
+        src: ['app/**'],
+        dest: 'dist/'
+      },
+      favicon: {
+        src: 'src/img/favicon.ico',
+        dest: 'dist/'
+      },
+      fonts: {
+        expand: true,
+        cwd: 'src/fonts/',
+        src: ['**'],
+        dest: 'dist/fonts/'
+      },
+      html: {
+        expand: true,
+        cwd: 'src/html/',
+        src: ['**'],
+        dest: 'dist/'
+      }
+    },
+    autoprefixer: {
+        options: {
+            browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1', 'ie 9']
+        },
+        dist: {
+          expand: true,
+          flatten: true,
+          src: 'dist/css/*.css',
+          dest: 'dist/css/'
+        },
+    },
     clean: {
+      less: 'dist/css/less.css',
       dist: 'dist/*'
     },
     concat: {
       options: {
-        separator: ';'
+        banner: '<%= banner %>\n',
+        stripBanners: false
       },
       css: {
         src: ['src/css/**/*.css'],
         dest: 'dist/css/<%= pkg.name %>.css'
       },
       js: {
+        options: {
+          banner: '<%= banner %>\n<%= jqueryCheck %>\n'
+        },
         src: ['src/js/**/*.js'],
         dest: 'dist/js/<%= pkg.name %>.js'
       }
     },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          base: '.',
+          livereload: true
+        }
+      }
+    },
+    csscomb: {
       dist: {
         files: {
-          'dist/js/<%= pkg.name %>.min.js': ['<%= concat.js.dest %>']
+          'dist/css/<%= pkg.name %>.css': 'dist/css/<%= pkg.name %>.css',
+          'dist/css/less.css': 'dist/css/less.css'
         }
       }
     },
@@ -47,88 +125,43 @@ module.exports = function(grunt) {
         dest: 'dist/css/<%= pkg.name %>.min.css'
       }
     },
-    csscomb: {
+    htmlbuild: {
       dist: {
-        files: {
-          'dist/css/<%= pkg.name %>.css': 'dist/css/<%= pkg.name %>.css',
-          'dist/css/less.css': 'dist/css/less.css'
+        src: 'index.html',
+        dest: 'dist/',
+        options: {
+          beautify: true,
+          relative: true,
+          styles: {
+            bundle: [
+              '**/*.css'
+            ]
+          },
+          scripts: {
+            app: '**/*.js'
+          }
         }
       }
     },
-    copy: {
-      app: {
-        expand: true,
-        cwd: 'src/',
-        src: ['app/**'],
-        dest: 'dist/'
-      },
-      fonts: {
-        expand: true,
-        cwd: 'src/fonts/',
-        src: ['**'],
-        dest: 'dist/fonts/'
-      },
-      html: {
-        expand: true,
-        cwd: 'src/html/',
-        src: ['**'],
-        dest: 'dist/'
-      },
-      bootstrap: {
-        expand: true,
-        cwd: 'assets/bootstrap/dist/',
-        src: ['**'],
-        dest: 'dist/assets/bootstrap/'
-      },
-      ng: {
-        expand: true,
-        cwd: 'assets/angular.js/build/',
-        src: ['**'],
-        dest: 'dist/assets/ng/'
+    imagemin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'src/', 
+          src: ['img/**/*.{png,jpg,gif}'],
+          dest: 'dist/'
+        }]
       }
     },
-    htmlbuild: {
-		dist: {
-			src: 'index.html',
-			dest: 'dist/',
-			options: {
-				beautify: true,
-				relative: true,
-				styles: {
-                    bundle: [
-                        '**/*.css'
-                    ]
-                },
-				scripts: {
-					app: '**/*.js'
-				}
-			}
-		}
-	},
-	imagemin: {
-		dist: {
-			files: [{
-				expand: true,
-				cwd: 'src/', 
-				src: ['img/**/*.{png,jpg,gif}'],
-				dest: 'dist/'
-			}]
-		}
-	},
-	less: {
-		dist: {
-			options: {
-				cleancss: true,
-			},
-			files: {
-				"dist/css/less.css": "src/less/<%= pkg.name %>.less"
-			}
-		}
-	},
-    qunit: {
-      files: ['test/**/*.html']
+    jscs: {
+      grunt: {
+        src: 'Gruntfile.js'
+      },
+      src: {
+        src: 'src/js/*.js'
+      },
     },
-    jshint: {
+	  jshint: {
       dist: ['Gruntfile.js', 'src/js/**/*.js', 'test/**/*.js'],
       options: {
         // options here to override JSHint defaults
@@ -140,15 +173,39 @@ module.exports = function(grunt) {
         }
       }
     },
+    less: {
+      dist: {
+        options: {
+          cleancss: true,
+        },
+        files: {
+          "dist/css/less.css": "src/less/<%= pkg.name %>.less"
+        }
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/js/<%= pkg.name %>.min.js': ['<%= concat.js.dest %>']
+        }
+      }
+    },
     watch: {
       files: ['<%= jshint.dist %>','src/**/*'],
-      tasks: ['jshint', 'qunit']
+      tasks: ['dist']
     }
   });
-  require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
 
-  //grunt.registerTask('test', ['jshint', 'qunit']);
-
-  grunt.registerTask('default', ['clean', 'copy','imagemin', 'jshint', 'concat', 'less', 'csscomb', 'cssmin', 'uglify', 'htmlbuild']);
-
+  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('assets', ['exec', 'copy']);
+  grunt.registerTask('js', ['concat:js', 'uglify']);
+  grunt.registerTask('css', ['less', 'concat:css', 'clean:less', 'autoprefixer', 'csscomb', 'cssmin']);
+  grunt.registerTask('static', ['copy', 'imagemin', 'htmlbuild']);
+  grunt.registerTask('dist', ['clean:dist', 'js', 'css', 'static']);
+  grunt.registerTask('serve', ['dist','connect','watch']);
+  grunt.registerTask('all', ['assets','test','dist']);
+  grunt.registerTask('default', ['dist']);
 };
